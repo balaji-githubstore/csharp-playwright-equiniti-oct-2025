09fe710838aa1207c7ed3310ef54dac9518074b0
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-10 19:26:38 +0530

    Second commit

--- PlaywrightAutomation2/Demo6Frame.cs
+++ PlaywrightAutomation2/Demo6Frame.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+//Frame
 namespace EQ.PlaywrightAutomation
 {
     public class Demo6Frame
