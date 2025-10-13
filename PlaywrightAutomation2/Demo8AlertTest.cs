b8917f8fb65cabb5909b8bce61f36613b315c3b9
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-13 18:20:26 +0530

    Added code for alert 2

--- PlaywrightAutomation2/Demo8AlertTest.cs
+++ PlaywrightAutomation2/Demo8AlertTest.cs
@@ -10,7 +10,7 @@ namespace PlaywrightAutomation
     public class Demo8AlertTest
     {
         [Test]
-        public async Task HdfcLoginAlertTest()
+        public async Task HdfcLoginAlertTestAsync()
         {
             var playwright = await Playwright.CreateAsync();
             var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false, Channel = "chrome" });
@@ -20,16 +20,40 @@ namespace PlaywrightAutomation
             await page.GotoAsync("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm", new() { WaitUntil = WaitUntilState.Load, Timeout = 0 });
 
             string actualAlertMessage = null;
-
-            page.Dialog +=  (_, dialog) =>
+            //register the event handler to work on alert box
+            page.Dialog += async (_, dialog) =>
             {
                 actualAlertMessage = dialog.Message;
-               await dialog.AcceptAsync();
+                await dialog.AcceptAsync();
             };
-            
+
             //click on Go
             //auto handles the alert - use dialog event handler to get alert text
             await page.Locator("//img[@alt='Go']").ClickAsync();
+           
+            Console.WriteLine(actualAlertMessage);
+            await Task.Delay(5000);
+        }
+
+        [Test]
+        public async Task NasscomAlertTestAsync()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false, Channel = "chrome" });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://www.nasscom.in/nasscom-membership", new() { WaitUntil = WaitUntilState.Load, Timeout = 0 });
+
+            string actualAlertMessage = null;
+
+            page.Dialog += async (_, dialog) =>
+            {
+                actualAlertMessage = dialog.Message;
+                await dialog.AcceptAsync();
+            };
+
+            await page.Locator("xpath=//a[text()='Calculate Fee']").ClickAsync();
 
             Console.WriteLine(actualAlertMessage);
             await Task.Delay(5000);
@@ -37,4 +61,3 @@ namespace PlaywrightAutomation
     }
 }
 
-//will start at 6:05 PM IST (in 15 mins)
